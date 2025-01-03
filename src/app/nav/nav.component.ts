b338933b976ac6a7375329ce08d3afca03a42a7e
yasel <lesaywd@gmail.com>
2025-01-03 09:35:34 -0300

    integrando material tolbar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,MatMenuModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
